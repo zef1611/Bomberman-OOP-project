4fde77b2a8f176422e61a713b26c6b40bc20fda5
Tri Vy <trivyluong@gmail.com>
2021-10-02 11:27:44 +0700

    turn Game into ApplicationAdapter

--- test/core/src/com/mygdx/game/Bomberman.java
+++ test/core/src/com/mygdx/game/Bomberman.java
@@ -8,7 +8,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-public class Bomberman extends Game {
+public class Bomberman extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	private Texture background;
